8f43d74ca415df2128aaa4ad7ce539d5065e6473
avaoleh <avakimyanov@gmail.com>
2025-06-30 18:21:26 +0300

    рактикуемся в использовании API клиентов

--- clients/files/files_client.py
+++ clients/files/files_client.py
@@ -5,6 +5,24 @@ from httpx import Response
 from clients.api_client import APIClient
 from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
 
+
+# Добавили описание структуры файла
+class File(TypedDict):
+    """
+    Описание структуры файла.
+    """
+    id: str
+    url: str
+    filename: str
+    directory: str
+
+# Добавили описание структуры запроса на создание файла
+class CreateFileResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания файла.
+    """
+    file: File
+
 class CreateFileRequestDict(TypedDict):
     """
     Описание структуры запроса на создание файла.
@@ -49,6 +67,11 @@ class FilesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/files/{file_id}")
+
+    # Добавили новый метод
+    def create_file(self, request: CreateFileRequestDict) -> CreateFileResponseDict:
+        response = self.create_file_api(request)
+        return response.json()
 
 # Добавляем builder для FilesClient
 def get_files_client(user: AuthenticationUserDict) -> FilesClient:
--- clients/users/public_users_client.py
+++ clients/users/public_users_client.py
@@ -23,6 +23,25 @@ class CreateUserRequestDict(TypedDict):
     middleName: str
 
 
+# Добавили описание структуры пользователя
+class User(TypedDict):
+    """
+    Описание структуры пользователя.
+    """
+    id: str
+    email: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+
+# Добавили описание структуры ответа создания пользователя
+class CreateUserResponseDict(TypedDict):
+    """
+    Описание структуры ответа создания пользователя.
+    """
+    user: User
+
 class PublicUsersClient(APIClient):
     """
     Клиент для работы с публичным эндпоинтом /api/v1/users.
@@ -37,6 +56,11 @@ class PublicUsersClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response.
         """
         return self.post("/api/v1/users", json=request)
+
+    # Добавили новый метод
+    def create_user(self, request: CreateUserRequestDict) -> CreateUserResponseDict:
+        response = self.create_user_api(request)
+        return response.json()
 
 # Добавляем builder для PublicUsersClient
 def get_public_users_client() -> PublicUsersClient:
