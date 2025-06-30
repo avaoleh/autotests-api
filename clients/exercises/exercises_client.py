fa8e8fee3d4ff8f708f9fc49a457892a227e1b02
avaoleh <avakimyanov@gmail.com>
2025-06-30 18:38:03 +0300

    рактикуемся в использовании API клиентов

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -1,9 +1,23 @@
-from typing import TypedDict
+from typing import TypedDict, List
 
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.private_http_builder import AuthenticationUserDict, get_private_http_client
 
+class Exercise(TypedDict):
+    id: str
+    title: str
+    courseId: str
+    maxScore: int
+    minScore: int
+    orderIndex: int
+    description: str
+    estimatedTime: str
+
+
+class GetExercisesResponseDict(TypedDict):
+    exercises: List[Exercise]
 
 class GetExercisesQueryDict(TypedDict):
     """
@@ -90,3 +104,28 @@ class ExercisesClient(APIClient):
         :return: Ответ от сервера в виде объекта httpx.Response
         """
         return self.delete(f"/api/v1/exercises/{exercise_id}")
+
+    # Полезные методы, возвращающие структурированные данные
+    def get_exercises(self, query: GetExercisesQueryDict) -> GetExercisesResponseDict:
+        response = self.get_exercises_api(query)
+        return response.json()
+
+    def get_exercise(self, exercise_id: str) -> Exercise:
+        response = self.get_exercise_api(exercise_id)
+        return response.json()['exercise']
+
+    def create_exercise(self, request: CreateExerciseRequestDict) -> dict:
+        response = self.create_exercise_api(request)
+        return response.json()
+
+    def update_exercise(self, exercise_id: str, request: UpdateExerciseRequestDict) -> dict:
+        response = self.update_exercise_api(exercise_id, request)
+        return response.json()
+
+# Добавляем builder для ExercisesClient
+def get_exercises_client(user: AuthenticationUserDict) -> ExercisesClient:
+    """
+    Функция создаёт экземпляр ExercisesClient с уже настроенным HTTP-клиентом.
+    :return: Готовый к использованию ExercisesClient.
+    """
+    return ExercisesClient(client=get_private_http_client(user))
